2f42ab52a5ee996637276b065c18aab569f130a4
NickAdkins <d00263885@dmail.dixie.edu>
2018-05-29 15:41:50 -0600

    built project. First commit

---
+++ config.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase-firestore.js"></script>
+
+
+<script>
+    var config = {
+        apiKey: "AIzaSyAk9cRB97ruVtZwRhP_qopD32Mfn9t6LCc",
+        authDomain: "javascriptstudyprod.firebaseapp.com",
+        databaseURL: "https://javascriptstudyprod.firebaseio.com",
+        projectId: "javascriptstudyprod",
+        storageBucket: "javascriptstudyprod.appspot.com",
+        messagingSenderId: "562305102808"
+};
+firebase.initializeApp(config);
+</script>
+
+
